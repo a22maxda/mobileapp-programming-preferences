1401e364490a32908d25ce1925923ec2922e9bc8
a22maxda <a22maxda@student.his.se>
2024-04-29 11:51:35 +0200

    fixat gradle, lagt till MyActivity.java och lagt till widgets

---
+++ app/src/main/java/com/example/project/MyActivity.java
@@ -0,0 +1,31 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MyActivity extends AppCompatActivity {
+
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+
+        TextView prefTextRef = findViewById(R.id.prefText);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found"));
+    }
+
+    public void savePref(View view) {
+
+    }
+}
