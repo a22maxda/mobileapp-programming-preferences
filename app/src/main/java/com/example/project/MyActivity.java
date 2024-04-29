d5570663359f960c63ecc03162e74899346d28cf
a22maxda <a22maxda@student.his.se>
2024-04-29 12:02:34 +0200

    Lagt till ändring av text i MyActivity.java

--- app/src/main/java/com/example/project/MyActivity.java
+++ app/src/main/java/com/example/project/MyActivity.java
@@ -2,21 +2,22 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MyActivity extends AppCompatActivity {
 
     private SharedPreferences myPreferenceRef;
     private SharedPreferences.Editor myPreferenceEditor;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-
 
         myPreferenceRef = getPreferences(MODE_PRIVATE);
         myPreferenceEditor = myPreferenceRef.edit();
@@ -26,6 +27,14 @@ public class MyActivity extends AppCompatActivity {
     }
 
     public void savePref(View view) {
+        EditText newPrefText = findViewById(R.id.settingseditview);
 
+        myPreferenceEditor.putString("MyAppPreferenceString", newPrefText.getText().toString());
+        myPreferenceEditor.apply();
+
+        TextView prefTextRef = findViewById(R.id.prefText);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+
+        newPrefText.setText("");
     }
 }
